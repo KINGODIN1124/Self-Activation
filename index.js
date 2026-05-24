1b8af51d8049366487e2a433301dce47b6bdea38
unknown <shivanshu.mishra2580@gmail.com>
2026-05-24 14:51:57 +0530

    Add index.js master launcher entrypoint for deployment

---
+++ index.js
@@ -0,0 +1,47 @@
+const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+console.log('[MASTER LAUNCHER] Starting services...');
+
+// Launch Discord Bot
+console.log('[MASTER LAUNCHER] Launching Discord Bot...');
+const discordBot = spawn('node', ['botDiscord.js'], { stdio: 'inherit' });
+
+discordBot.on('close', (code) => {
+    console.error(`[MASTER LAUNCHER] Discord bot exited with code ${code}`);
+});
+
+// Launch Steam Worker
+console.log('[MASTER LAUNCHER] Launching Steam Worker...');
+const steamWorker = spawn('node', ['StemTokenGen.js'], { stdio: 'inherit' });
+
+steamWorker.on('close', (code) => {
+    console.error(`[MASTER LAUNCHER] Steam worker exited with code ${code}`);
+});
+
+// Launch Ubisoft Worker (if dll and dotnet are available)
+const ubiDir = path.join(__dirname, 'UbisoftBot');
+const ubiDll = path.join(ubiDir, 'DenuvoTicket.dll');
+
+if (fs.existsSync(ubiDll)) {
+    console.log('[MASTER LAUNCHER] Ubisoft bot files detected. Checking for dotnet environment...');
+    const checkDotnet = spawn('dotnet', ['--version']);
+    
+    checkDotnet.on('close', (code) => {
+        if (code === 0) {
+            console.log('[MASTER LAUNCHER] dotnet runtime found. Launching Ubisoft worker...');
+            const ubiWorker = spawn('dotnet', ['DenuvoTicket.dll'], { cwd: ubiDir, stdio: 'inherit' });
+            
+            ubiWorker.on('close', (c) => {
+                console.error(`[MASTER LAUNCHER] Ubisoft worker exited with code ${c}`);
+            });
+        } else {
+            console.warn('[MASTER LAUNCHER] dotnet command was not found. Skipping Ubisoft worker. (Ensure .NET Runtime is installed if hosting Ubisoft titles)');
+        }
+    });
+
+    checkDotnet.on('error', () => {
+        console.warn('[MASTER LAUNCHER] dotnet command failed or was not found. Skipping Ubisoft worker.');
+    });
+}
